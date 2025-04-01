0e6ccfb82838970319031b21a300dc56e93f7b39
tonovarela <tonovarela@live.com>
2025-04-01 17:10:31 -0600

    Actualizar la API de semillas y mejorar el componente TodosItem para incluir nuevos elementos y estilos de visualización

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -5,9 +5,11 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
 
     await prisma.todo.deleteMany({}); // Delete all todos
+
    const todo= await prisma.todo.createMany({
-        data: [{
-            description: "Learn to code",complete:true},
+        data: [
+            {description: "Learn to code",complete:true},
+            {description: "Learn to HTML",complete:true},
             {description: "Build a project"},
             {description: "Deploy the project"},
             {description: "Celebrate!"},
--- src/todos/componentes/TodosItem.tsx
+++ src/todos/componentes/TodosItem.tsx
@@ -1,15 +1,30 @@
 import { Todo } from '@prisma/client';
 import styles from './TodosItem.module.css';
+import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 
 interface Props {
   todo: Todo;
 }
 
-export const TodosItem = ({todo}:Props) => {
-  return (    
-    <div className={styles.todoPending}>
-      {todo.description}
+export const TodosItem = ({ todo }: Props) => {
+  return (
+    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
+      <div className="flex flex-col sm:flex-row justify-start items-center gap-4 ">
+        <div className={` flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 bg-blue-100 
+          ${todo.complete ? 'bg-blue-100' : 'bg-red-100'} `}>          
+          {
+            todo.complete ?
+              <IoCheckboxOutline size={30} className='text-blue-400' /> :
+              <IoSquareOutline size={30} className='text-gray-400' />
+          }
+          
+          <div className='text-center sm:text-left'>
+            {todo.description}
+          </div>
+        </div>
+
+      </div>
     </div>
-        
+
   )
 }
